7255eeb74b3ce9027e7e30231883ff4ec5d319e1
alexasir513 <150673281+alexasir513@users.noreply.github.com>
2023-11-13 12:18:55 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Script que genera un árbol de directorios
+REM
+REM Alejandro Nevado Maldonado
+REM 13-11-2023
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti 
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\exam
